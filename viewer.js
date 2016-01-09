ef929b9d1225aa798b7208fdd19edf0823d71ed9
toshi0607 <s.toshi0607@gmail.com>
2016-01-09 23:56:08 +0900

    ~p149

---
+++ viewer.js
@@ -0,0 +1,11 @@
+﻿function revBtnClick(){
+  document.getElementById('no').innerHTML = 'No.1';
+  document.getElementById('pict').src = 'img/1.jpg';
+  document.getElementById('no').className = 'no1';
+}
+
+function fwdBtnClick(){
+  document.getElementById('no').innerHTML = 'No.2';
+  document.getElementById('pict').src = 'img/2.jpg';
+  document.getElementById('no').className = 'no2';
+}
