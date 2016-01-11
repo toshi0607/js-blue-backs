965e682ac4b66755f159423afb005e0f5de6b005
toshi0607 <s.toshi0607@gmail.com>
2016-01-11 11:13:49 +0900

    ~p268

--- viewer.js
+++ viewer.js
@@ -1,40 +1,44 @@
-var pictNo = 1;
-var myImg = [];
+window.onload = init;
 
-for (var i = 0;i < 4; i++){
-  myImg[i] = document.createElement('img');
-  myImg[i].src = 'img/' + (i + 1) + '.jpg';
-}
+function init(){
+  var pictNo = 1;
+  var myImg = [];
 
-function showImg(){
-  document.getElementById('no').innerHTML = 'No.' + pictNo;
-  document.getElementById('pict').src = myImg[pictNo - 1].src;
-}
-
-document.getElementById('rev_btn').onclick = revBtnClick;
-
-function revBtnClick(){
-  pictNo--;
-  showImg();
-
-  if (pictNo === 1) {
-    document.getElementById('fwd_btn').disabled = 'disabled';
-  } else if (pictNo === myImg.length - 1) {
-    document.getElementById('fwd_btn').disabled = false;
-    document.getElementById('no').className = 'no1';
+  for (var i = 0;i < 4; i++){
+    myImg[i] = document.createElement('img');
+    myImg[i].src = 'img/' + (i + 1) + '.jpg';
   }
-}
 
-document.getElementById('fwd_btn').onclick = fwdBtnClick;
+  function showImg(){
+    document.getElementById('no').innerHTML = 'No.' + pictNo;
+    document.getElementById('pict').src = myImg[pictNo - 1].src;
+  }
 
-function fwdBtnClick(){
-  pictNo++;
-  showImg();
+  document.getElementById('rev_btn').onclick = revBtnClick;
 
-  if (pictNo === myImg.length) {
-    document.getElementById('no').className = 'no2';
-    document.getElementById('fwd_btn').disabled = 'disabled';
-  } else if (pictNo === 2) {
-    document.getElementById('rev_btn').disabled = false;
+  function revBtnClick(){
+    pictNo--;
+    showImg();
+
+    if (pictNo === 1) {
+      document.getElementById('fwd_btn').disabled = 'disabled';
+    } else if (pictNo === myImg.length - 1) {
+      document.getElementById('fwd_btn').disabled = false;
+      document.getElementById('no').className = 'no1';
+    }
+  }
+
+  document.getElementById('fwd_btn').onclick = fwdBtnClick;
+
+  function fwdBtnClick(){
+    pictNo++;
+    showImg();
+
+    if (pictNo === myImg.length) {
+      document.getElementById('no').className = 'no2';
+      document.getElementById('fwd_btn').disabled = 'disabled';
+    } else if (pictNo === 2) {
+      document.getElementById('rev_btn').disabled = false;
+    }
   }
 }
