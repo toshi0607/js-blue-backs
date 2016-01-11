c4a159af3e79425d044d2df5a9e635eb665542f8
toshi0607 <s.toshi0607@gmail.com>
2016-01-11 09:42:15 +0900

    ~p199

--- viewer.js
+++ viewer.js
@@ -8,6 +8,8 @@ function revBtnClick(){
 
   if (pictNo === 1) {
     document.getElementById('fwd_btn').disabled = 'disabled';
+  } else if (pictNo === 3) {
+    document.getElementById('fwd_btn').disabled = false;
   }
 }
 
@@ -18,5 +20,7 @@ function fwdBtnClick(){
   if (pictNo === 4) {
     document.getElementById('no').className = 'no2';
     document.getElementById('fwd_btn').disabled = 'disabled';
+  } else if (pictNo === 2) {
+    document.getElementById('rev_btn').disabled = false;
   }
 }
